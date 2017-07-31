b4b3a848161f75c3735465840b8bf341e6bfbb2c
Renato Siqueira <30542215+RenatoSiqueira@users.noreply.github.com>
2017-07-31 15:37:32 -0300

    Update gulpfile.js
    Retirando pontuação desnecessária

--- gulpfile.js
+++ gulpfile.js
@@ -19,20 +19,20 @@ gulp.task('gerar-css',function(){
             .pipe(sass())                           // Conversão SASS->CSS
             .pipe(concatenar(nomeCSS))              // Unificação dos Arquivos CSS
             .pipe(minifyCSS({ext:'.min.css'}))      // Minificação + Alteração Nome.min.css
-            .pipe(gulp.dest('./dist/css'));         // Salvando no diretório final
+            .pipe(gulp.dest('./dist/css'))          // Salvando no diretório final
 })
 
 // HTML->HTML [Minificação,Salvar]
 gulp.task('gerar-html',function(){
     return gulp.src(html)
             .pipe(minifyHTML())                     // Minificação
-            .pipe(gulp.dest('./dist/'));            // Salvando no diretório final
+            .pipe(gulp.dest('./dist/'))             // Salvando no diretório final
 })
 
 // OBSERVADORES
 gulp.task('monitor',function(){
-    gulp.watch(scss,['gerar-css']);                 // Observando os arquivos SASS
-    gulp.watch(html,['gerar-html']);                // Observando os arquivos HTML
+    gulp.watch(scss,['gerar-css'])                  // Observando os arquivos SASS
+    gulp.watch(html,['gerar-html'])                 // Observando os arquivos HTML
 })
 
 // Inicia as funções e Ativa os Observadores
